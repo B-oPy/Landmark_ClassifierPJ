136ab48e7c81558dd461d45a95f3437ebf6f3cd3
anisepy <heesu940@gmail.com>
2022-07-19 16:56:33 +0900

    feat : make the Flask file for vgg16 model of landmark image set

--- Flask/README.md
+++ Flask/README.md
@@ -1,1 +1,15 @@
-# Landmark_ClassifierPJ
+## Landmark Classifier
+
+> This classifier can classify 12 Lanmarks, 'burj_khalifa','chichen_itza','christ_the_reedemer','eiffel_tower','great_wall_of_china', 'machu_pichu','pyramids_of_giza','roman_colosseum','statue_of_liberty','stonehenge','taj_mahal', and 'venezuela_angel_falls'.
+
+### Image Set Source
+
+<img width="80%" src="./img/imageset.png"/>
+
+* This imageset was taken from [Wonders of the World Image Dataset] (https://www.kaggle.com/datasets/balabaskar/wonders-of-the-world-image-classification)
+
+### First Deep Learning Model : VGG16
+
+<img width="80%" src="https://neurohive.io/wp-content/uploads/2018/11/vgg16-1-e1542731207177.png"/>
+
+* We use this model for first Deep Learning model making.
---
+++ Flask/app.py
@@ -0,0 +1,93 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = '딥러닝 프로젝트/landmark_Flask_Bopy/model/010-0.1822-0.9444-0.2505-0.9222.hdf5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+model.make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+# from keras.applications.resnet50 import ResNet50
+# model = ResNet50(weights='imagenet')
+# model.save('ResNet50_modle.h5')
+print('Model loaded. Check http://127.0.0.1:5001/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        # preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        # result = str(pred_class[0][0][1])               # Convert to string
+        # return result
+        landmark = ['burj_khalifa','chichen_itza','christ_the_reedemer','eiffel_tower','great_wall_of_china',
+                    'machu_pichu','pyramids_of_giza','roman_colosseum','statue_of_liberty','stonehenge',
+                    'taj_mahal','venezuela_angel_falls']
+        classes = model_predict(file_path, model)
+        for i in range(len(classes[0])) :
+            if classes[0][i]>0.5:
+                result = landmark[i]
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
+
