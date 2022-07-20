b5099f43ad04d651d97a4223d8cc3b25bb835440
anisepy <heesu940@gmail.com>
2022-07-20 15:19:44 +0900

    fix : Fix css&JS option,and printing label&prediction percentage

--- Flask/app.py
+++ Flask/app.py
@@ -5,6 +5,7 @@ import os
 import glob
 import re
 import numpy as np
+import pandas as pd
 
 # Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
@@ -20,19 +21,11 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH = '딥러닝 프로젝트/landmark_Flask_Bopy/model/010-0.1822-0.9444-0.2505-0.9222.hdf5'
+MODEL_PATH = './MODEL/010-0.1822-0.9444-0.2505-0.9222.hdf5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
 model.make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-# from keras.applications.resnet50 import ResNet50
-# model = ResNet50(weights='imagenet')
-# model.save('ResNet50_modle.h5')
-print('Model loaded. Check http://127.0.0.1:5001/')
 
 
 def model_predict(img_path, model):
@@ -70,20 +63,18 @@ def upload():
         f.save(file_path)
 
         # Make prediction
-        # preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        # result = str(pred_class[0][0][1])               # Convert to string
-        # return result
         landmark = ['burj_khalifa','chichen_itza','christ_the_reedemer','eiffel_tower','great_wall_of_china',
                     'machu_pichu','pyramids_of_giza','roman_colosseum','statue_of_liberty','stonehenge',
                     'taj_mahal','venezuela_angel_falls']
         classes = model_predict(file_path, model)
-        for i in range(len(classes[0])) :
-            if classes[0][i]>0.5:
-                result = landmark[i]
+        import operator
+
+        result ={}
+        for i in range(12) :
+            result[landmark[i]] = round(classes[0][i]*100,4)
+        r1 = sorted(result.items(), key=operator.itemgetter(1), reverse=True)
+        result= str(r1[0][0]) + ' : ' + str(r1[0][1]) + '%\n' + str(r1[1][0]) + ' : ' + str(r1[1][1]) + '%\n' + str(r1[2][0]) + ' : ' + str(r1[2][1]) + '%'
+
         return result
     return None
 
